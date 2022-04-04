bbfc8cde4f3372c95eb142d5eb0d25861a39b6ef
logicupgrade <logicupgrade@DESKTOP-754Q8NC.localdomain>
2022-04-04 16:27:12 -0700

    added files and projects urls.py from Mingyuan

--- files/urls.py
+++ files/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.filesView, name='files'),
     path('new_file/', views.newFile, name='new_file'),
+    path('del_file/<int:file_id>/', views.del_file, name='del_file'),
     path('<path:filepath>/', views.file_response_download1, name='download'),
 ]
--- projects/urls.py
+++ projects/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('project/<int:project_id>/', views.singleProjectView, name='project'), #add ID here
     path('new_project/', views.createProjectView, name='create_project'),
     path('new_task/', views.addTask, name='new_task'),
+    path('add_members/<int:project_id>/<int:user_id>/', views.addMember, name='add_member'),
+    path('del_members/<int:project_id>/<int:user_id>/', views.delMember, name='del_member'),
 ]
